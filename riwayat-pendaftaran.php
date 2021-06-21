1c430e17bb32b8ef4a511545609a98107f7ab0a0
rulyadhika <rulihanif@gmail.com>
2021-06-21 16:14:46 +0700

    adding registration history page file, and default image

---
+++ riwayat-pendaftaran.php
@@ -0,0 +1,90 @@
+<?php
+$title = "Riwayat Pendaftaran Relawan";
+require_once './includes/header.php';
+require_once './db/connect.php';
+
+$id_pengguna = $_SESSION['id_pengguna'];
+// untuk ambil jumlah row apakah lebih dari 0
+$query = $pdo->query("SELECT COUNT(id_pengguna) as id FROM pengguna JOIN status USING(id_pengguna) WHERE pengguna.id_pengguna = $id_pengguna");
+$row = $query->fetch();
+// total row
+$total = $row['id'];
+
+$menampilkanDataPerHalaman = 5;
+$jumlahHalaman = ceil($total / $menampilkanDataPerHalaman);
+$halamanAktif = isset($_GET['halaman']) ? $_GET['halaman'] : 1;
+$awalData = ($menampilkanDataPerHalaman * $halamanAktif) - $menampilkanDataPerHalaman;
+
+// semua data diambil
+$results = $pdo->query("SELECT status.status,acara.judul_acara,acara.lokasi,acara.id_acara,jenis_acara.nama_jenis_acara,organisasi.nama AS nama_org FROM pengguna JOIN status USING(id_pengguna) 
+                        JOIN acara USING(id_acara) JOIN jenis_acara USING(id_jenis_acara) JOIN organisasi USING(id_organisasi) WHERE pengguna.id_pengguna = $id_pengguna
+                        LIMIT $awalData, $menampilkanDataPerHalaman");
+
+?>
+
+<!-- Judul -->
+<h1 class="text-center mb-4"><?= $title; ?></h1>
+
+<h6>Total Pendaftaran : <?= $total; ?> Acara</h6>
+
+<!-- Tabel -->
+<?php if ($total > 0) : ?>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover">
+            <thead class="table-dark align-middle">
+                <tr>
+                    <th scope="col" class="text-center">No.</th>
+                    <th scope="col" class="text-center">Acara</th>
+                    <th scope="col" class="text-center">Jenis Acara</th>
+                    <th scope="col" class="text-center">Penyelenggara</th>
+                    <th scope="col" class="text-center">Lokasi</th>
+                    <th scope="col" class="text-center">Status</th>
+                    <th scope="col" class="text-center">Aksi</th>
+                </tr>
+            </thead>
+            <tbody class="align-middle">
+                <?php
+                $i = ($menampilkanDataPerHalaman * $halamanAktif) - ($menampilkanDataPerHalaman - 1);
+                while ($r = $results->fetch(PDO::FETCH_ASSOC)) :
+                ?>
+                    <tr>
+                        <th scope="row" class="text-center"><?= $i; ?></th>
+                        <td><?= $r['judul_acara']; ?></td>
+                        <td><?= $r['nama_jenis_acara']; ?></td>
+                        <td><?= $r['nama_org']; ?></td>
+                        <td><?= $r['lokasi']; ?></td>
+                        <td class="fw-bold text-center text-<?= $r['status'] === 'lolos' ? 'success' : ($r['status'] === 'gagal' ? 'danger' : 'warning'); ?>"><?= ucfirst($r['status']); ?></td>
+                        <td class="text-center">
+                            <a href="./viewaktivitas.php?id_acara=<?= $r['id_acara']; ?>" class="btn btn-sm btn-danger">Lihat Acara</a>
+                        </td>
+                    </tr>
+                <?php $i++;
+                endwhile ?>
+            </tbody>
+        </table>
+    </div>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+            <li class="page-item <?php if ($halamanAktif == 1) echo 'disabled'; ?>">
+                <a class="page-link" href="halaman=<?= $halamanAktif - 1 ?>" tabindex="-1" aria-disabled=<?= $halamanAktif == 1 ? "true" : "false"; ?>>Previous</a>
+            </li>
+            <?php
+            $i = $halamanAktif < 4 ? 1 : $halamanAktif - 3;
+            for ($i; $i <= $jumlahHalaman; $i++) :
+            ?>
+                <li class="page-item <?php if ($halamanAktif == $i) echo 'active'; ?>" <?php if ($halamanAktif == $i) echo 'aria-current="page"'; ?>>
+                    <a class="page-link" href="?halaman=<?= $i ?>"><?= $i; ?></a>
+                </li>
+            <?php endfor ?>
+            <li class="page-item <?php if ($halamanAktif == $jumlahHalaman) echo 'disabled'; ?>">
+                <a class="page-link" href="halaman=<?= $halamanAktif + 1 ?>">Next</a>
+            </li>
+        </ul>
+    </nav>
+<?php else : ?>
+    <h3 style="text-align: center;">Belum ada data yang masuk!</h3>
+<?php endif ?>
+
+<?php
+require './includes/footer.php'
+?>
